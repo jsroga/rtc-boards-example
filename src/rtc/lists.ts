b4938ab418b5de990f9ca955d587ad15462b27d2
Jacek Sroga <jaceksroga@Jaceks-MacBook-Pro-2.local>
2019-10-14 20:50:34 +0200

    linting

--- src/rtc/lists.ts
+++ src/rtc/lists.ts
@@ -1,18 +1,24 @@
-import { IListState } from '../store/reducers/lists';
-import { IAddListOp, IRemoveListOp, IChangeListOrderOp } from './types/lists';
+import { IListState } from '../store/reducers/lists'
+import { IAddListOp, IRemoveListOp, IChangeListOrderOp } from './types/lists'
 
-export const shareAddList = (list: IListState): IAddListOp[] => [{
-  p: ["lists", list.id],
-  oi: list
-}]
+export const shareAddList = (list: IListState): IAddListOp[] => [
+  {
+    p: ['lists', list.id],
+    oi: list
+  }
+]
 
-export const shareRemoveList = (list: IListState): IRemoveListOp[] => [{
-  p: ["lists", list.id],
-  od: list
-}]
+export const shareRemoveList = (list: IListState): IRemoveListOp[] => [
+  {
+    p: ['lists', list.id],
+    od: list
+  }
+]
 
-export const shareChangeListOrder = (list: IListState, order: number): IChangeListOrderOp[] => [{
-    p: ["lists", list.id, "order"],
-  od: list.order,
-  oi: order
-}]
+export const shareChangeListOrder = (list: IListState, order: number): IChangeListOrderOp[] => [
+  {
+    p: ['lists', list.id, 'order'],
+    od: list.order,
+    oi: order
+  }
+]
